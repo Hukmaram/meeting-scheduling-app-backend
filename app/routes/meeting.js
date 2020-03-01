671630929941d35f7333500e59c1eda67f114133
Hukmaram-Bishnoi <engineerhukmaram29@gmail.com>
2020-03-01 16:00:43 +0530

    API doc

--- app/routes/meeting.js
+++ app/routes/meeting.js
@@ -11,10 +11,204 @@ module.exports.setRouter = (app) => {
 
 
     app.post(`${baseUrl}/addMeeting`, meetingController.addMeetingFunction);
-    
+     /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/meetings/addMeeting api to Add Meeting.
+     *
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * @apiParam {string} meetingTopic Topic of the Meeting. (body params) (required)
+     * @apiParam {string} hostId User Id of the user hosting Meeting. (body params) (required)
+     * @apiParam {string} hostName User Name of the user hosting Meeting. (body params) (required)
+     * @apiParam {string} participantId User Id of the participant. (body params) (required)
+     * @apiParam {string} participantName User Name of the participant. (body params) (required)
+     * @apiParam {string} participantEmail Email of the participant. (body params) (required)
+     * @apiParam {string} meetingStartDate Start date/time of Meeting. (body params) (required)
+     * @apiParam {string} meetingEndDate end date/time of Meeting. (body params) (required)
+     * @apiParam {string} meetingDescription Description of Meeting. (body params) (required)
+     * @apiParam {string} meetingPlace Place of Meeting. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Meeting Created",
+            "status": 200,
+            "data": {
+                "__v": 0,
+                "_id": "5b9a2581d1508e15f402fb36",
+                "createdOn": "2018-09-13T08:53:21.000Z",
+                "meetingPlace": "Izbita",
+                "meetingDescription": "Test Meeting for Checking",
+                "meetingEndDate": "2018-09-12T17:57:50.382Z",
+                "meetingStartDate": "2018-09-11T20:30:00.000Z",
+                "participantEmail": "Dhruv@gmail.com",
+                "participantName": "Dhruv Bishnoi",
+                "participantId": "Cdcyuc8OX",
+                "hostName": "Hukmaram Bishnoi",
+                "hostId": "Cdcyuc8OX",
+                "meetingTopic": "Test Meeting",
+                "meetingId": "rJttBsw_m"
+            }
+        }
+    */
     app.get(`${baseUrl}/view/all/meetings/:userId`, meetingController.getAllMeetings);
+   /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {get} /api/v1/meetings/view/all/meetings/:userId api for Getting all Meetings of User.
+     *
+     * @apiParam {string} userId userId of the user. (query params) (required)
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * 
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Meetings Found",
+            "status": 200,
+            "data": [
+                {
+                    "_id": "5b9a2581d1508e15f402fb36",
+                    "createdOn": "2018-09-13T08:53:21.000Z",
+                    "meetingPlace": "Sarswati Palace",
+                    "meetingDescription": "Test Meeting for Updating",
+                    "meetingEndDate": "2018-09-12T17:57:50.382Z",
+                    "meetingStartDate": "2018-09-11T20:30:00.000Z",
+                    "participantEmail": "dhruv@gmail.com",
+                    "participantName": "Dhruv Bishnoi",
+                    "participantId": "B1cyuc8OX",
+                    "hostName": "Hukmaram Bishnoi",
+                    "hostId": "B1cyuc8OX",
+                    "meetingTopic": "Test Meeting",
+                    "meetingId": "rJttBsw_m",
+                    "__v": 0
+                }
+            ]
+        }
+    */
+
+
     app.post(`${baseUrl}/:meetingId/delete`, meetingController.deleteMeetingFunction);
+     /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/meetings/:meetingId/delete api to Delete Meeting.
+     *
+     * @apiParam {string} meetingId meetingId of the Meeting to be deleted. (query params) (required)
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Meeting Deleted",
+            "status": 200,
+            "data": {
+                "error": false,
+                "message": "Deleted the Meeting successfully",
+                "status": 200,
+                "data": {
+                    "_id": "5b9a2dac036ca203dc49534f",
+                    "__v": 0,
+                    "createdOn": "2018-09-13T09:28:12.000Z",
+                    "meetingPlace": "Sarsawati",
+                    "meetingDescription": "Test Meeting for Updating",
+                    "meetingEndDate": "2018-09-12T17:57:50.382Z",
+                    "meetingStartDate": "2018-09-11T20:30:00.000Z",
+                    "participantEmail": "Dhruv@gmail.com",
+                    "participantName": "Dhruv Bishnoi",
+                    "participantId": "Cdcyuc8OX",
+                    "hostName": "Hukmaram Bishnoi",
+                    "hostId": "B1cyuc8OX",
+                    "meetingTopic": "Test Meeting",
+                    "meetingId": "rkHhTovdm"
+                }
+            }
+        }
+    */
+
     app.get(`${baseUrl}/:meetingId/details`, meetingController.getMeetingDetailsFunction);
+     /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {get} /api/v1/meetings/:meetingId/details api for getting meeting details.
+     *
+     * @apiParam {string} meetingId meetingId of the Meeting. (query params) (required)
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        { 
+            "error": false,
+            "message": "Meeting Found",
+            "status": 200,
+            "data": {
+                "_id": "5b9a2581d1508e15f402fb36",
+                "createdOn": "2018-09-13T08:53:21.000Z",
+                "meetingPlace": "Sarswati",
+                "meetingDescription": "Test Meeting for Updating",
+                "meetingEndDate": "2018-09-12T17:57:50.382Z",
+                "meetingStartDate": "2018-09-11T20:30:00.000Z",
+                "participantEmail": "dhruv@gmail.com",
+                "participantName": "Dhruv Bishnoi",
+                "participantId": "Cdcyuc8OX",
+                "hostName": "Hukmaram Bishnoi",
+                "hostId": "B1cyuc8OX",
+                "meetingTopic": "Test Meeting",
+                "meetingId": "rJttBsw_m",
+                "__v": 0
+            }
+        }
+    */
     app.put(`${baseUrl}/:meetingId/updateMeeting`, meetingController.updateMeetingFunction);
+     /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {put} /api/v1/meetings/:meetingId/updateMeeting api to Update Meeting Details.
+     *
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * @apiParam {string} meetingId Id of the Meeting. (query params) (required)
+     * @apiParam {string} meetingTopic Topic of the Meeting. (body params) (required)
+     * @apiParam {string} meetingStartDate Start date/time of Meeting. (body params) (required)
+     * @apiParam {string} meetingEndDate end date/time of Meeting. (body params) (required)
+     * @apiParam {string} meetingDescription Description of Meeting. (body params) (required)
+     * @apiParam {string} meetingPlace Place of Meeting. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Meeting Updated",
+            "status": 200,
+            "data": {
+                "error": false,
+                "message": "Meeting details Updated",
+                "status": 200,
+                "data": "None"
+            }
+        }
+    */
     app.post(`${baseUrl}/admin-meetings/sentReminders`, meetingController.sendReminderForTodaysMeetings);
+    /**
+     * @apiGroup Meetings
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/meetings/admin-meetings/sentReminders api for sending reminders to the users of the meeting.
+     *
+     * @apiParam {string} userId userId of the User. (body params) (required)
+     * @apiParam {string} authToken Authentication Token. (body/header/query params) (required)
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        { 
+            "error": false,
+            "message": "Meetings Found and reminders sent",
+            "status": 200,
+            "data": null
+        }
+    */
 }
+/** Run this command : apidoc -i app/routes/ -o apidoc/ */
--- app/routes/user.js
+++ app/routes/user.js
@@ -7,11 +7,196 @@ module.exports.setRouter=(app)=>{
     let baseUrl = `${appConfig.apiVersion}/users`;
     
     app.post(`${baseUrl}/signup`,userController.signupFunction);
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/signup api for Registering User.
+     *
+     * @apiParam {string} firstName First Name of the user. (body params) (required)
+     * @apiParam {string} lastname Last Name of the user. (body params) (required)
+     * @apiParam {string} userName userName of the user. (body params) (required)
+     * @apiParam {string} countryName country Name of the user. (body params) (required)
+     * @apiParam {string} mobileNumber Mobile Number of the user. (body params) (required)
+     * @apiParam {string} admin String(true/false) true-if user is admin and false-if user is not admin. (body params) (required)
+     * @apiParam {string} email email of the user. (body params) (required)
+     * @apiParam {string} password password of the user. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+        "error": false,
+        "message": "User Created",
+        "status": 200,
+        "data": [
+            {
+                "createdOn": "2018-09-12T13:42:58.000Z",
+                "validationToken": "Null",
+                "email": "hukmaram@gmail.com",
+                "password": "$2a$10$XvHxf9JX76JvvIeqwd2CoOdxtCraX23nR2ToAYIhynLmNquDFdbOa",
+                "admin": "false",
+                "mobileNumber": "91 9074748389",
+                "countryName": "India",
+                "userName": "hukmaram bishnoi",
+                "lastName": "Bishnoi",
+                "firstName": "Hukmaram",
+                "userId": "Cdcyuc8OX"
+            }
+        }
+    */
     app.post(`${baseUrl}/login`,userController.loginFunction);
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/login api for Login.
+     *
+     * @apiParam {string} email email of the user. (body params) (required)
+     * @apiParam {string} password password of the user. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Login Successful",
+            "status": 200,
+            "data": {
+                "authToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqd3RpZCI6InJrVW51b2pPbSIsImlhdCI6MTUzNzA5MTc1NzU1NywiZXhwIjoxNTM3MTc4MTU3LCJzdWIiOiJhdXRoVG9rZW4iLCJpc3MiOiJsZXRzTWVldEFwcCIsImRhdGEiOnsiZW1haWxWZXJpZmllZCI6IlllcyIsInZhbGlkYXRpb25Ub2tlbiI6Ik51bGwiLCJlbWFpbCI6InNheXllZHNvZnR0ZWNoMzEzQGdtYWlsLmNvbSIsImlzQWRtaW4iOiJ0cnVlIiwibW9iaWxlTnVtYmVyIjoiOTEgNzg0MDk2Mjg4NyIsImNvdW50cnlOYW1lIjoiSW5kaWEiLCJ1c2VyTmFtZSI6IlNoYWgtYWRtaW4iLCJsYXN0TmFtZSI6IlNheXllZCIsImZpcnN0TmFtZSI6IlNoYWhydWtoIiwidXNlcklkIjoiQjFjeXVjOE9YIn19.fcCu0TZQ-WnAs8bOmZa9YhF1YVv2JscTwOPT--rTwbc",
+                "userDetails": {
+                    "validationToken": "Null",
+                    "email": "hukmaram@gmail.com",
+                    "isAdmin": "true",
+                    "mobileNumber": "91 7368547488",
+                    "countryName": "India",
+                    "userName": "Hukmaram Bishnoi",
+                    "lastName": "Bishnoi",
+                    "firstName": "Hukmaram",
+                    "userId": "Cdcyuc8OX"
+                }
+            }
+        }    
+    */
+
     app.post(`${baseUrl}/forgotPassword`,userController.forgotPasswordFunction);
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/forgotPassword api for Password Reset.
+     *
+     * @apiParam {string} email email of the user. (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Password reset instructions sent successfully",
+            "status": 200,
+            "data": None
+        }    
+    */
     app.post(`${baseUrl}/resetpassword`,userController.updatePasswordFunction);
+  /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {put} /api/v1/users/resetPassword api for Updating Password after Reset.
+     *
+     * @apiParam {string} validationToken validationToken of the user recieved on Email. (body params) (required)
+     * @apiParam {string} password new password of the user . (body params) (required)
+     *
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Password Update Successfully",
+            "status": 200,
+            "data": "None"
+            
+        }
+    */
     app.get(`${baseUrl}/view/all`, userController.getAllUser);
+    
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {get} /api/v1/users/view/all api for Getting all users.
+     *
+     * @apiParam {string} authToken authToken of the user. (query/body/header params) (required)
+     * 
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "All User Details Found",
+            "status": 200,
+            "data": [
+                {
+                    "createdOn": "2018-09-12T13:42:58.000Z",
+                    "validationToken": "Null",
+                    "email": "hukmaram@gmail.com",
+                    "password": "$2a$10$XvHxf9JX76JvvIeqwd2CoOdxtCraX23nR2ToAYIhynLmNquDFdbOa",
+                    "admin": "true",
+                    "mobileNumber": "91 9484848922",
+                    "countryName": "India",
+                    "userName": "Hukmaram Bishnoi",
+                    "lastName": "Bishnoi",
+                    "firstName": "Hukmaram",
+                    "userId": "Cdcyuc8OX"
+                }
+            ]
+        }
+    */
+
     app.get(`${baseUrl}/view/:userId`, userController.getSingleUser);
+    /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {get} /api/v1/users/:userId/details api for getting user details.
+     *
+     * @apiParam {string} userId userId of the user. (query params) (required)
+     * @apiParam {string} authToken authToken of the user. (query/body/header params) (required)
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "User Details Found",
+            "status": 200,
+            "data": {
+                "createdOn": "2018-09-12T13:42:58.000Z",
+                "validationToken": "Null",
+                "email": "hukmaram@gmail.com",
+                "password": "$2a$10$XvHxf9JX76JvvIeqwd2CoOdxtCraX23nR2ToAYIhynLmNquDFdbOa",
+                "admin": "true",
+                "mobileNumber": "91 9837373728",
+                "countryName": "India",
+                "userName": "hukmaram Bishnoi",
+                "lastName": "Bishnoi",
+                "firstName": "Hukmaram",
+                "userId": "Cdcyuc8OX"
+            }
+        }
+    */
     app.post(`${baseUrl}/:userId/logout`, userController.logout);
+
+      /**
+     * @apiGroup users
+     * @apiVersion  1.0.0
+     * @api {post} /api/v1/users/:userId/logout api to logout from application.
+     *
+     * @apiParam {string} userId userId of the user. (query params) (required)
+     * @apiParam {string} authToken authToken of the user. (query/body/header params) (required)
+     * @apiSuccess {object} myResponse shows error status, message, http status code, result.
+     * 
+     * @apiSuccessExample {object} Success-Response:
+        {
+            "error": false,
+            "message": "Logged Out Successfully",
+            "status": 200,
+            "data": null
+        }
+    */
     
 }
